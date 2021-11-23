15ca5a3a8ee6e51a4e02d717270c11c4eaa4bdac
sebsan89 <seb.sannac@gmail.com>
2021-11-23 14:32:45 +0100

    moving models, admin and views in separate applications. Moving data from old tables to new ones. Deletion of old models and corresponding tables

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 
-from . import views
+from lettings.views import letting, lettings_index
+from profiles.views import profile, profiles_index
+from .views import index
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
+    path('', index, name='index'),
+    path('lettings/', lettings_index, name='lettings_index'),
+    path('lettings/<int:letting_id>/', letting, name='letting'),
+    path('profiles/', profiles_index, name='profiles_index'),
+    path('profiles/<str:username>/', profile, name='profile'),
     path('admin/', admin.site.urls),
 ]
