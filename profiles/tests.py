4db07479125f3eabd3baf0dfcac3797fe138ce5b
sebsan89 <seb.sannac@gmail.com>
2021-11-29 17:54:59 +0100

    create unit test for profiles

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from django.urls import reverse, resolve
+from django.test import Client
+from django.contrib.auth.models import User
+from profiles.models import Profile
+
+
+@pytest.mark.django_db
+def test_profiles_url():
+    User.objects.create(username='test',
+                        password='azerty1234!',
+                        last_name='nom de famille',
+                        email="test@test.com")
+    Profile.objects.create(user_id='1',
+                           favorite_city='Paris')
+    path = reverse('profile', kwargs={'username': 'test'})
+
+    assert path == "/profiles/test/"
+    assert resolve(path).view_name == "profile"
+
+
+@pytest.mark.django_db
+def test_profiles_view():
+    client = Client()
+    User.objects.create(username='test',
+                        password='azerty1234!',
+                        last_name='nom de famille',
+                        email="test@test.com")
+    Profile.objects.create(user_id='1',
+                           favorite_city='Paris')
+    path = reverse('profile', kwargs={'username': 'test'})
+    response = client.get(path)
+    content = response.content.decode()
+    expected_content_last_name = "<p>Last name: nom de famille</p>"
+    expected_content_email = "<p>Email: test@test.com</p>"
+
+    assert expected_content_last_name in content
+    assert expected_content_email in content
+    assert response.status_code == 200
